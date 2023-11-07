e707d76cc2016491d2cabc2150055e74ab886ec2
kat9.rud <kat9.rud@gmail.com>
2023-11-07 15:48:02 +0100

    added Java Faker test data

---
+++ src/test/java/data/TestData.java
@@ -0,0 +1,61 @@
+package data;
+
+import com.github.javafaker.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+
+public class TestData {
+    Faker faker = new Faker();
+    private final String[] hobbies = new String[]{"Sports", "Reading", "Music"};
+    private final String[] subjects = new String[]{"Math", "Chemistry", "Physics", "Computer Science",
+            "English", "History"};
+    private final String[] states = new String[]{"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+
+    public String firstName = faker.name().firstName();
+    public String lastName = faker.name().lastName();
+    public String email = faker.internet().emailAddress();
+    public String gender = faker.demographic().sex();
+    public String mobile = String.valueOf(faker.number().numberBetween(9151111111L, 9269999999L));
+    private String[] dayMonthYear = getRandomDate();
+    public String dayOfBirth = String.format("%s", dayMonthYear[0]);
+    public String monthOfBirth = String.format("%s", dayMonthYear[1]);
+    public String yearOfBirth = String.format("%s", dayMonthYear[2]);
+    public String subject = getRandomValue(subjects);
+    public String hobby = getRandomValue(hobbies);
+    public String currentAddress = faker.address().streetAddress();
+    public String state = getRandomValue(states);
+    public String city = getCity();
+    public String pic = "user-logo.png";
+
+    private String getRandomValue(String... initialValues) {
+        return faker.options().option(initialValues);
+    }
+
+    private String getCity() {
+        String cityItem = null;
+
+        if (state.equals("NCR")) {
+            cityItem = getRandomValue("Delhi", "Gurgaon", "Noida");
+        }
+
+        if (state.equals("Uttar Pradesh")) {
+            cityItem = getRandomValue("Agra", "Lucknow", "Merrut");
+        }
+
+        if (state.equals("Haryana")) {
+            cityItem = getRandomValue("Karnal", "Panipat");
+        }
+
+        if (state.equals("Rajasthan")) {
+            cityItem = getRandomValue("Jaipur", "Jaiselmer");
+        }
+
+
+        return cityItem;
+    }
+    private String[] getRandomDate() {
+        return new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(faker.date().birthday(18, 70)).split(" ");
+    }
+}
--- src/test/java/tests/StudentRegistrationWPageObjectsTests.java
+++ src/test/java/tests/StudentRegistrationWPageObjectsTests.java
@@ -1,50 +1,37 @@
 package tests;
 
+import data.TestData;
 import org.junit.jupiter.api.Test;
 
 public class StudentRegistrationWPageObjectsTests extends TestBase {
+    TestData data = new TestData();
     @Test
     void fillStudentRegistrationFormTest() {
-        var firstName = "Anne";
-        var lastName = "Smith";
-        var email = "anne.smith@example.com";
-        var gender = "Female";
-        var mobile = "7823526297";
-        var dayOfBirth = "01";
-        var monthOfBirth = "January";
-        var yearOfBirth = "1990";
-        var subject = "Physics";
-        var hobby = "Sports";
-        var currentAddress = "7539 W Dallas St";
-        var state = "NCR";
-        var city = "Delhi";
-        var pic = "user-logo.png";
-
         page.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setPhone(mobile)
-                .setBirthday(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubjects(subject)
-                .setHobbies(hobby)
-                .uploadPicture(pic)
-                .setCurrentAddress(currentAddress)
-                .setState(state)
-                .setCity(city)
+                .setFirstName(data.firstName)
+                .setLastName(data.lastName)
+                .setEmail(data.email)
+                .setGender(data.gender)
+                .setPhone(data.mobile)
+                .setBirthday(data.dayOfBirth, data.monthOfBirth, data.yearOfBirth)
+                .setSubjects(data.subject)
+                .setHobbies(data.hobby)
+                .uploadPicture(data.pic)
+                .setCurrentAddress(data.currentAddress)
+                .setState(data.state)
+                .setCity(data.city)
                 .submit();
 
         page.verifyResultsModalAppears()
-                .verifyResults("Student Name", firstName + " " + lastName)
-                .verifyResults("Student Email", email)
-                .verifyResults("Gender", gender)
-                .verifyResults("Mobile", mobile)
-                .verifyResults("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .verifyResults("Subjects", subject)
-                .verifyResults("Hobbies", hobby)
-                .verifyResults("Picture", pic)
-                .verifyResults("Address", currentAddress)
-                .verifyResults("State and City", state + " " + city);
+                .verifyResults("Student Name", data.firstName + " " + data.lastName)
+                .verifyResults("Student Email", data.email)
+                .verifyResults("Gender", data.gender)
+                .verifyResults("Mobile", data.mobile)
+                .verifyResults("Date of Birth", data.dayOfBirth + " " + data.monthOfBirth + "," + data.yearOfBirth)
+                .verifyResults("Subjects", data.subject)
+                .verifyResults("Hobbies", data.hobby)
+                .verifyResults("Picture", data.pic)
+                .verifyResults("Address", data.currentAddress)
+                .verifyResults("State and City", data.state + " " + data.city);
     }
 }
